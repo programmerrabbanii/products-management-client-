a13419b74c7dd9a90a0c244121dfc295058baf6c
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-11-28 17:34:24 +0600

    added the why choose us

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import BlogSection from "../components/BlogSection";
+import FeaturesSection from "../components/FeaturesSection";
 
 const Home = () => {
     return (
         <div>
            <BlogSection></BlogSection>
+           <FeaturesSection></FeaturesSection>
         </div>
     );
 };
